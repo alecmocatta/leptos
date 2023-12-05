fbf3838e2c55c709441bbe8269bb78c94a4aa3dd
Giovanni <88086644+Giovanni-Tably@users.noreply.github.com>
2023-12-05 14:20:52 +0000

    Modify `std::fmt::Debug` impls

--- leptos_reactive/src/memo_nodedup.rs
+++ leptos_reactive/src/memo_nodedup.rs
@@ -102,11 +102,10 @@ where
 
 impl<T> Copy for MemoNodedup<T> {}
 
-impl<T> fmt::Debug for MemoNodedup<T> {
+impl<T: fmt::Debug> fmt::Debug for MemoNodedup<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut s = f.debug_struct("MemoNodedup");
-        s.field("id", &self.id);
-        s.field("ty", &self.ty);
+        self.with_untracked(|v| s.field("inner", v));
         #[cfg(any(debug_assertions, feature = "ssr"))]
         s.field("defined_at", &self.defined_at);
         s.finish()
