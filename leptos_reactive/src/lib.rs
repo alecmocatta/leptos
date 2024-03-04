e000120472ac43a73227fcb0f550be339d463a08
Giovanni <88086644+Giovanni-Tably@users.noreply.github.com>
2024-03-04 13:36:05 +0000

    Rename `signal_ext` -> `signal_more`
    To avoid using the same module name as internally.

--- leptos_reactive/src/lib.rs
+++ leptos_reactive/src/lib.rs
@@ -84,7 +84,7 @@ extern crate tracing;
 mod signal;
 pub mod callback;
 mod context;
-mod signal_ext;
+mod signal_more;
 #[macro_use]
 mod diagnostics;
 mod effect;
@@ -136,7 +136,7 @@ pub use runtime::{
 pub use selector::*;
 pub use serialization::*;
 pub use signal::{prelude as signal_prelude, *};
-pub use signal_ext::*;
+pub use signal_more::*;
 pub use signal_wrappers_read::*;
 pub use signal_wrappers_write::*;
 pub use slice::*;
